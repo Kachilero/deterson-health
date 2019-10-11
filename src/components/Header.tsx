c0519dec05922fdfb94dda13a9ba169d1ed4fe5e
Alejandro <alejcerro@gmail.com>
2019-10-11 06:29:57 -0500

    Moved some things around and set up SASS

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+/**
+ * Header to be displayed on all pages
+ */
+import React from 'react';
+
+export const Header = () => (
+  <header className="App-header">
+    <div className="content-container">
+      <img src="../images/logo.svg" className="App-logo" alt="logo" />
+    </div>
+  </header>
+);
+
+export default Header;
